01b005bca64ebab69b1486216db918fb4e41e344
Eric Roberts <notthebmovieactor@gmail.com>
2023-11-28 14:59:29 -0500

    Clean up build and add minimum Python version of 3.9

--- build/lib/umap/__init__.py
+++
@@ -1,6 +0,0 @@
-from get_kmers import GetKmers
-from run_bowtie import BowtieWrapper
-from unify_bowtie import UnifyBowtie
-from combine_umaps import CombineUmaps
-from uint8_to_bed import Int8Handler
-import map_bed
